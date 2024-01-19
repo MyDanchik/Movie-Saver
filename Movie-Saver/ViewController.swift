cbc37aeb7efe5f6704b0b0435b39f64f846b15af
Daniil <idanik.mega@gmail.com>
2024-01-19 11:22:26 +0300

    add file and gitignore

---
+++ Movie-Saver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-Saver
+//
+//  Created by Daniil Yushkevich on 19.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
